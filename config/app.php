c8b931f097943bc5f35f27ab21236d2a7f12a26a
Mirza O <oglecevacmirza@gmail.com>
2019-04-09 17:33:52 +0200

    Added debug blacklist

--- config/app.php
+++ config/app.php
@@ -2,16 +2,32 @@
 
 return [
 
+        'debug_blacklist' => [
+            '_ENV' => [
+                'APP_KEY',
+                'DB_PASSWORD',
+            ],
+
+            '_SERVER' => [
+                'APP_KEY',
+                'DB_PASSWORD',
+            ],
+
+            '_POST' => [
+                'password',
+            ]
+        ],
+
     /*
-    |--------------------------------------------------------------------------
-    | Application Name
-    |--------------------------------------------------------------------------
-    |
-    | This value is the name of your application. This value is used when the
-    | framework needs to place the application's name in a notification or
-    | any other location as required by the application or its packages.
-    |
-    */
+        |--------------------------------------------------------------------------
+        | Application Name
+        |--------------------------------------------------------------------------
+        |
+        | This value is the name of your application. This value is used when the
+        | framework needs to place the application's name in a notification or
+        | any other location as required by the application or its packages.
+        |
+        */
 
     'name' => env('APP_NAME', 'Laravel'),
 
